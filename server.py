ccc8b41aa3f1fd208088d697b0e56f2f356cbc6d
mitch3x3 <mitch3x3@github.com>
2017-10-14 16:34:33 -0700

    Adding client and server socket scripts

---
+++ server.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import json
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('localhost', 8089))
+serversocket.listen(5) # become a server socket, maximum 5 connections
+
+while True:
+    connection, address = serversocket.accept()
+    buf = connection.recv(64)
+    if len(buf) > 0:
+        d = json.loads(buf)
+        print d
+        # break
