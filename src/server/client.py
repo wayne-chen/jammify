86f0207795dea07d44da846553a3895fed498c25
mitch3x3 <mitch3x3@github.com>
2017-10-14 17:00:02 -0700

    Class-ified the server & client sockets

--- src/server/client.py
+++ src/server/client.py
@@ -3,10 +3,33 @@ import sys
 import json
 import socket
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('localhost', 8089))
+# clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# clientsocket.connect(('localhost', 8089))
 
-sample_json_message = {"a": "123"}
-sample_json_string = json.dumps(d)
+# sample_json_message = {"a": "123"}
+# sample_json_string = json.dumps(d)
 
-clientsocket.send(sample_json_string)
+# clientsocket.send(sample_json_string)
+
+
+class MessageClient(object):
+    def __init__(self, ip_address='localhost', port=8089):
+        self.port = port
+
+        self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clientsocket.connect((ip_address, port))
+    
+    def send(self, message):
+        self.clientsocket.send(message)
+
+
+def main():
+    client = MessageClient(ip_address='localhost', port=8089)
+
+    sample_json_message = {"a": "123"}
+    sample_json_string = json.dumps(sample_json_message)
+
+    client.send(message=sample_json_string)
+
+if __name__ == '__main__':
+    main()
